1fcdabae73d682a011e49b67db8ae474915105c7
oonoyosp <oonoyosp@systena.co.jp>
2017-04-13 15:35:56 +0900

    fix Copyright name for apple doc

--- ObjcFirstApp/RankingViewController.m
+++ ObjcFirstApp/RankingViewController.m
@@ -3,7 +3,7 @@
 //  ObjcFirstApp
 //
 //  Created by Natsumo Ikeda on 2016/06/27.
-//  Copyright © 2016年 NIFTY Corporation. All rights reserved.
+//  Copyright 2017 FUJITSU CLOUD TECHNOLOGIES LIMITED All Rights Reserved.
 //
 
 #import "RankingViewController.h"
